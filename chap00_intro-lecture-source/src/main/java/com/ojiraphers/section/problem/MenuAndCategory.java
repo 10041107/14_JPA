dc55d707c1b19ac209d24eb9292b4fef63c93a4e
20231126 <aksmfdyfl111@gmail.com>
2024-02-01 18:42:41 +0900

    main

---
+++ chap00_intro-lecture-source/src/main/java/com/ojiraphers/section/problem/MenuAndCategory.java
@@ -0,0 +1,16 @@
+package com.ojiraphers.section.problem;
+
+public class MenuAndCategory {
+
+    public static MenuAndCategory menuAndCategory;
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+
+    private Category category;
+
+    private String orderableStatus;
+
+
+
+}
