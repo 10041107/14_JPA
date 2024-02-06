b8b2bc775bcd21af610f2f87f6a7f71bb09b4233
20231126 <aksmfdyfl111@gmail.com>
2024-02-06 18:26:51 +0900

    main

---
+++ chap03_association_mapping/src/test/java/com/ohgiraffers/section02/onetomany/CategoryAndMenu.java
@@ -0,0 +1,98 @@
+package com.ohgiraffers.section02.onetomany;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "category_section02")
+@Table(name = "tbl_category")
+public class CategoryAndMenu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "ref_category_code")
+    private String refCategoryCode;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "category_code")
+    private List<Menu> menuList;
+
+
+    /*
+    @OneToMany(mappedBy = "categoryAndMenu", .....)
+    mappedBy: 양방향 매핑을 할때 사용됨.
+    one to many등의 한방향 매핑시 사용하면 stackoverflow오류 발생
+    (재귀함수 무한 호출하게됨)
+     */
+
+    public CategoryAndMenu() {
+    }
+
+    public CategoryAndMenu(int categoryCode, String categoryName, String refCategoryCode, List<Menu> menuList) {
+        this.categoryCode = categoryCode;
+        this.categoryName = categoryName;
+        this.refCategoryCode = refCategoryCode;
+        this.menuList = menuList;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getRefCategoryCode() {
+        return refCategoryCode;
+    }
+
+    public void setRefCategoryCode(String refCategoryCode) {
+        this.refCategoryCode = refCategoryCode;
+    }
+
+//    public List<Menu> getMenuList() {
+//        return menuList;
+//    }
+//
+//    public void setMenuList(List<Menu> menuList) {
+//        this.menuList = menuList;
+//    }
+
+//    public List<Menu> getmenuList(){return menuList;}
+//
+//    public void setMenuList(List<Menu> menuList){
+//        List<Menu> newList = new ArrayList<>();
+//        for (Menu m: menuList){
+//            m.setCategory(this.categoryCode);
+//            newList.add(m);
+//        }
+//        this.menuList = newList;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "CategoryAndMenu{" +
+                "categoryCode=" + categoryCode +
+                ", categoryName='" + categoryName + '\'' +
+                ", refCategoryCode=" + refCategoryCode +
+                ", menuList=" + menuList +
+                '}';
+    }
+}
---
+++ chap03_association_mapping/src/test/java/com/ohgiraffers/section02/onetomany/OneToManyAssocationTests.java
@@ -0,0 +1,83 @@
+package com.ohgiraffers.section02.onetomany;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneToManyAssocationTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+//    @Test
+//    void 일대다_연관관계_객체_그래프_탐색을_이용한_조회_테스트(){
+//        int categoryCode = 10;
+//
+//        CategoryAndMenu categoryAndMenu = entityManager.find(CategoryAndMenu.class, categoryCode);
+//
+//        //then
+//        Assertions.assertNotNull(categoryAndMenu);
+//
+//        System.out.println(categoryAndMenu);
+//    }
+//
+//    @Test
+//    void 일대다_연관관계_객체_삽입_테스트(){
+//        CategoryAndMenu categoryAndMenu = new CategoryAndMenu();
+//        categoryAndMenu.setCategoryCode(902);
+//        categoryAndMenu.setCategoryName("일대다 카테고리");
+//        categoryAndMenu.setRefCategoryCode(null);
+//
+//        List<Menu> menuList = new ArrayList<>();
+//        Menu menu = new Menu();
+//        menu.setMenuCode(777);
+//        menu.setMenuName("일대다 아이스크림");
+//        menu.setMenuPrice(8700);
+//        menu.setOrderableStatus("y");
+//        menu.setCategoryAndMenu(categoryAndMenu);
+//        /*
+//
+//         */
+//
+//        menuList.add(menu);
+//
+//        categoryAndMenu.setMenuList(menuList);
+//
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//        entityManager.persist(categoryAndMenu);
+//        transaction.commit();
+//        CategoryAndMenu foundCategoryMenu = entityManager.find(CategoryAndMenu.class, 902);
+//        System.out.println(foundCategoryMenu);
+//
+//    }
+//
+
+
+
+}
